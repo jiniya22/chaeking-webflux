79372a3c39d6f34395210f2069479cabac5eef08
jini <jini@jiniworld.me>
2022-09-22 17:43:39 +0900

    :tada:

---
+++ src/main/kotlin/me/jiniworld/book/ChaekingKotlinApplication.kt
@@ -0,0 +1,11 @@
+package me.jiniworld.book
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChaekingKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChaekingKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/me/jiniworld/book/ChaekingKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.jiniworld.book
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChaekingKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
