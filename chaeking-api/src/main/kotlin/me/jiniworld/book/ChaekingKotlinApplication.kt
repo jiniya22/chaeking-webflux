05dfcc00cf7333ad14bcd4d02e2f45a572415c9d
jini <jini@jiniworld.me>
2022-09-22 18:28:41 +0900

    :bug: locale, @EnableJpaAuditing 설정 추가

--- chaeking-api/src/main/kotlin/me/jiniworld/book/ChaekingKotlinApplication.kt
+++ chaeking-api/src/main/kotlin/me/jiniworld/book/ChaekingKotlinApplication.kt
@@ -2,10 +2,12 @@ package me.jiniworld.book
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.*
 
 @SpringBootApplication
 class ChaekingKotlinApplication
 
 fun main(args: Array<String>) {
+    Locale.setDefault(Locale.KOREA)
     runApplication<ChaekingKotlinApplication>(*args)
 }
---
+++ chaeking-api/src/main/kotlin/me/jiniworld/book/config/ApplicationConfig.kt
@@ -0,0 +1,8 @@
+package me.jiniworld.book.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class ApplicationConfig
