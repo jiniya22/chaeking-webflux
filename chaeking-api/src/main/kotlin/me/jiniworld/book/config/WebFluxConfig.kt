6dce1b737e873adf59b7af0b11e0f629da1259cc
jini <jini@jiniworld.me>
2022-10-18 16:23:36 +0900

    :wrench: WebFluxConfig 추가: HandlerMethodArgumentResolver 설정 추가

---
+++ chaeking-api/src/main/kotlin/me/jiniworld/book/config/WebFluxConfig.kt
@@ -0,0 +1,57 @@
+package me.jiniworld.book.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.MethodParameter
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.BindingContext
+import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.web.reactive.config.WebFluxConfigurer
+import org.springframework.web.reactive.result.method.HandlerMethodArgumentResolver
+import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
+
+@Configuration
+@EnableWebFlux
+class WebFluxConfig(
+    private val authUserHandlerArgumentResolver: AuthUserHandlerArgumentResolver,
+): WebFluxConfigurer {
+
+//    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+//        registry.addResourceHandler("/resources/**")
+//            .addResourceLocations("classpath:/static/")
+//            .setCacheControl(CacheControl.maxAge(3, TimeUnit.DAYS));
+//    }
+
+    override fun configureArgumentResolvers(configurer: ArgumentResolverConfigurer) {
+        configurer.apply {
+            addCustomResolver(authUserHandlerArgumentResolver)
+        }
+    }
+}
+
+
+@Component
+class AuthUserHandlerArgumentResolver : HandlerMethodArgumentResolver {
+
+    override fun supportsParameter(parameter: MethodParameter): Boolean =
+        AuthUser::class.java.isAssignableFrom(parameter.parameterType)
+
+    override fun resolveArgument(
+        parameter: MethodParameter,
+        bindingContext: BindingContext,
+        exchange: ServerWebExchange
+    ): Mono<Any> {
+        return AuthUser(
+            userId = 1,
+            username= "지니",
+        ).toMono()
+    }
+}
+
+data class AuthUser(
+    val userId: Long,
+    val username: String,
+    val profileUrl: String? = null,
+)
