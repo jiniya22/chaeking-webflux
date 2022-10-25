4a486c6852472900cac1d8a5a6be30cfb88f67c4
jini <jini@jiniworld.me>
2022-10-25 22:13:14 +0900

    :bug: kakao book 검색 webclient bug fix

--- chaeking-api/src/main/kotlin/me/jiniworld/book/client/KakaoWebClient.kt
+++ chaeking-api/src/main/kotlin/me/jiniworld/book/client/KakaoWebClient.kt
@@ -4,33 +4,31 @@ import kotlinx.coroutines.reactor.awaitSingle
 import me.jiniworld.book.model.client.KakaoBookRes
 import me.jiniworld.book.model.client.KakaoBookSearch
 import me.jiniworld.book.model.property.BookSearchProperties
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
-import org.springframework.web.util.UriComponentsBuilder
+import org.springframework.web.util.DefaultUriBuilderFactory
 
 @Component
 class KakaoWebClient(
     private val bookSearchProperties: BookSearchProperties,
 ) {
 
-    suspend fun searchBook(req: KakaoBookSearch): List<KakaoBookRes.Document> {
-        val uri = UriComponentsBuilder.fromPath("/v3/search/book")
-            .queryParam("query", req.query)
-            .queryParam("target", req.target)
-            .queryParam("sort", req.sort)
-            .queryParam("page", req.page)
-            .queryParam("size", req.size).build().toUri();
-        val authKey = "KakaoAK ${bookSearchProperties.kakao.apiKey}"
-        val tt =  WebClient.create("https://dapi.kakao.com").get()
-            .uri(uri)
-            .header("Authorization", authKey)
-//            .accept(MediaType.APPLICATION_JSON)
-                // onStatus(HttpStatus::is4xxClientError, response -> ...)
+    suspend fun searchBook(req: KakaoBookSearch): List<KakaoBookRes.Document> =
+        WebClient.create().get()
+            .uri(DefaultUriBuilderFactory("https://dapi.kakao.com")
+                .uriString("/v3/search/book")
+                .queryParam("query", req.query)
+                .queryParam("target", req.target)
+                .queryParam("sort", req.sort)
+                .queryParam("page", req.page)
+                .queryParam("size", req.size).build())
+            .header("Authorization", "KakaoAK ${bookSearchProperties.kakao.apiKey}")
+            .accept(MediaType.APPLICATION_JSON)
+            // onStatus(HttpStatus::is4xxClientError, response -> ...)
             .retrieve()
             .bodyToMono<KakaoBookRes>()
             .map { it.documents }
             .awaitSingle()
-        return tt
-    }
 }
